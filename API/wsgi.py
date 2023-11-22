330945dfe46c4c1ea838fd57b08a1b7c6b73b406
Alexis Chacon <66844946+OnlyAlec@users.noreply.github.com>
2023-11-22 02:11:52 -0600

    refactor: :lipstick: Add logging statement to wsgi.py

--- API/wsgi.py
+++ API/wsgi.py
@@ -5,4 +5,5 @@ if __name__ == "__main__":
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
-    app.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
+    app.logger.info('Init server!')
+    app.run()
